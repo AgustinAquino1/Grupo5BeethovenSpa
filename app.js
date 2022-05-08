c8e20334159304ea57295db573562daae746a566
RAIGLEITE <raienigleite1@gmail.com>
2022-05-08 19:44:58 -0300

    creacion register y login

--- app.js
+++ app.js
@@ -12,12 +12,21 @@ app.listen (port, () =>
 app.use(express.static(publicPath));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, './views/home.html'))   
+    res.sendFile(path.join(__dirname, './views/home.html'))
+})
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'))
+})
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, './views/register.html'))
 })
 
 app.get('*', (req, res) => {
     
     res.status(404).send("Not found 404")
     
-})
+});
+
 
