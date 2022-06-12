162497f6ea68dd0a17ebcb5e21a31005fa34e293
JuuHood <juuventre@gmail.com>
2022-06-12 18:39:49 -0300

    agregando archivos de la main

--- app.js
+++ app.js
@@ -1,28 +1,33 @@
 const express = require ('express');
 const app = express();
-const path = require ('path');
-const publicPath = path.resolve (__dirname, './public')
+const port = 3030
+const path = require ('path')
+const methodOverride = require('method-override');
 
-const port= 3030;
 
 
-app.listen (port, () =>
-    console.log('inicia el servidor'));
+const mainRouter = require ('./routers/main')
 
-app.use(express.static(publicPath));
+const productsRouter = require('./routers/products'); 
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, './views/home.html'))   
-});
-app.get("/productDetail", (req, res) => {
-    res.sendFile(path.join(__dirname, './views/productDetail.html'))   
-});
-app.get("/Cart", (req, res) => {
-    res.sendFile(path.join(__dirname, './views/productCart.html'))   
-});
-app.get('*', (req, res) => {
-    
-    res.status(404).send("Not found 404")
-    
+app.use(express.static(path.resolve(__dirname, './public')));
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
+
+app.set('view engine','ejs');
+
+app.set('views', path.resolve(__dirname, 'views'));
+
+app.use (methodOverride('_method'));
+
+
+app.use(express.static('public'));
+
+app.listen ( port, () => {
+    console.log ("servidor funcionando")
 })
 
+app.use("/", mainRouter)
+
+
+app.use('/products', productsRouter);
---
+++ routers/main.js
@@ -0,0 +1,19 @@
+const express = require ('express');
+const router = express.Router();
+const mainController = require ('../controllers/mainController')
+
+
+router.get("/", mainController.index );
+
+router.get("/login", mainController.login);
+
+
+
+router.get("/register", mainController.register);
+
+
+router.get('/search', mainController.search); 
+
+
+
+module.exports = router;
