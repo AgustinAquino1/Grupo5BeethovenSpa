2c98a3c899c907b832ad2a0abeeea6f7a38430b2
GabiDante <gabi.a.dante@gmail.com>
2022-08-24 23:28:41 -0300

    carrito 100% funcional desde el back al front

--- controllers/cartsController.js
+++ controllers/cartsController.js
@@ -3,6 +3,7 @@ const { validationResult } = require('express-validator');
 const db = require('../data/models');
 const sequelize = db.sequelize;
 const { Op } = require("sequelize");
+const { parse } = require('path');
 const Carts = db.Cart;
 
 
@@ -20,7 +21,9 @@ const controller = {
         }
         }).then(cart => {
             if (!cart) {
-                res.render('productCart',[])
+                res.render('productCart',{
+                    products : []                    
+                })
             } 
             else {
             
@@ -32,23 +35,40 @@ const controller = {
             }
 		      
         })  
-       //to do 
-       // bindear front end
+       
     },
 
     create: (req,res) => { 
+        
         const user = res.locals.userLogged
-       if (!user) {
+        if (!user) {
             res.redirect("/users/login")
         }
-        Carts.create({ 
-            user_id : user.id , 
-            products : req.body.products 
-
-        })
+        Carts.findOne({
+            where: { 
+                user_id: user.id
+            }
+        }).then(cart => {
+            if (!cart) { 
+                Carts.create({ 
+                    user_id : user.id , 
+                    products : JSON.stringify([req.body]) 
         
-        res.send(201)
+                })
+            }
+            else { 
+                const parseCart = JSON.parse(cart.products)
+                //const parseBody = JSON.parse(req.body)
+                parseCart.push(req.body)
+                cart.set({ 
+                    products : JSON.stringify(parseCart)
 
+                })
+                cart.save()
+            }
+            res.send(201)
+        })
+                        
     }
 
 }
