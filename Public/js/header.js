2815b55251c59c0729efe7968c102bd89172a0e0
AgustinAquino1 <agustin.aquino1@gmail.com>
2022-08-31 16:15:38 -0300

    creando menú desplegable de usuario

--- Public/js/header.js
+++ Public/js/header.js
@@ -1,10 +1,16 @@
 window.onload = ()=>{
 let barsMenu = document.querySelector(".fa-bars")
 let menuItems = document.querySelector(".burger")
+let avatarMenu = document.querySelector(".avatar")
+let menuItemsAvatar = document.querySelector(".avatarMenu")
 
     barsMenu.addEventListener("click", (e) =>{
       barsMenu.classList.toggle("rotate-bars")
       menuItems.classList.toggle("activate-menu-items")
+    })
+
+    avatarMenu.addEventListener("click", (e) =>{
+      menuItemsAvatar.classList.toggle("activate-menu-avatar-items")
     })
 
     }
